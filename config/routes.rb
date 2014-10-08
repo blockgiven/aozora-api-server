ebe017384da71bda8b72b46b77a650956246080a
block_given? <block_given@outlook.com>
2014-10-09 02:27:45 +0900

    Add /books/random/sentences/1.txt api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :books, only: [], id: /random/ do
+    resources 'sentences', only: :show, id: /1/
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
