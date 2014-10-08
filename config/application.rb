de947ba3eaf4e29c594024d319462c2a91a5729e
block_given? <block_given@outlook.com>
2014-10-09 03:05:35 +0900

    Revert "We need action_controller, action_view, sprockets, that's it."
    This reverts commit 06d6c49f4a7dbd46bda8d14bc3fa4f454044e399.

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails'
-%w(
-  action_controller
-  action_view
-  sprockets
-).each do |framework|
-  begin
-    require "#{framework}/railtie"
-  rescue LoadError
-  end
-end
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
